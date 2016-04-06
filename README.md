bbec2eb77c6e9100f8fd65b36446d8a2f728f2b9
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-05 21:43:19 -0700

    add futher complexity to the logic and add a README file.

---
+++ README.md
@@ -0,0 +1,31 @@
+# _Vacation Planner_
+
+#### _small js app for choosing a vacation destination based on quiz questions_  _4/03/2016_
+
+#### By _**Jeffrey Hackford**_
+
+## Description
+
+_{Application involving multiple choice questions in order to determine which destination you should visit}_
+
+## Setup/Installation Requirements
+
+* Clone from git hub
+* Open HTML file in any web browser
+
+## Known Bugs
+
+_{No known bugs at this time}_
+
+## Support and contact details
+
+_Jeffrey.Hackford@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, GitHub, Bootstrap, Javascript, Jquery_
+### License
+
+*MIT licensed*
+
+Copyright (c) 2016 **Jeffrey Hackford**
--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,25 @@ $(function() {
   $("#submit").click(function(event) {
 
     var operator = $("input:radio[name=operator]:checked").val();
+    var breakfast = $("input:radio[name=breakfast]:checked").val();
+    var destination = $("input:radio[name=moon]:checked").val();
 
     // $("output.destination1").hide(".myhidden");
     // $("output.destination2").hide(".myhidden");
     // $("output.destination3").hide(".myhidden");
 
-    if (operator === "flight") {
+    if (operator === "flight" && moon === "moon") {
       $(".destination1").removeClass("myhidden");
     }
-    if (operator === "read-minds") {
+    if (operator === "read-minds" && breakfast === "french-toast" || moon === "ocean") {
       $(".destination2").removeClass("myhidden");
     }
-    if (operator === "invisibility") {
+    if (operator === "invisibility" && moon === "moon") {
       $(".destination3").removeClass("myhidden");
+    }
+    else {
+      $(".destination4").removeClass("myhidden");
+
     }
     // $(".output").text(result);
     event.preventDefault();
